92bd492541df763d535e4e9523b69e6876cc8ace
owickstrom <oskar@wickstrom.tech>
2021-03-29 19:00:56 +0200

    Run with fake states

--- quickstrom/executor.py
+++ quickstrom/executor.py
@@ -7,24 +7,43 @@ class Check():
     def __init__(self, module: str):
         self.module = module
 
-
     def execute(self):
         with open("interpreter.log", "w+") as log:
-            with subprocess.Popen(["specstrom", "check", self.module], text=True, stdout=subprocess.PIPE, stderr=log, stdin=subprocess.PIPE, bufsize=1) as p:
+            with subprocess.Popen(["specstrom", "check", self.module], text=True, stdout=subprocess.PIPE, stderr=log, stdin=subprocess.PIPE, bufsize=0) as p:
 
                 def receive():
                     if p.poll() is None:
-                        print("Receiving")
-                        return json.loads(p.stdout.readline())
+                        msg = json.loads(p.stdout.readline())
+                        #print("Received", msg)
+                        return msg
                     else:
                         print("Done, can't receive.")
 
                 def send(msg):
-                    print("Sending")
+                    #print("Sending", msg)
                     if p.poll() is None:
                         p.stdin.write(json.dumps(msg) + '\n')
+                    else:
+                        print("Done, can't send.")
 
-                send({ 'tag': 'Event', 'contents': [{ 'tag': 'Loaded' }, { 'foo': [{ 'disabled': False }], 'bar': [{ 'disabled': False }], 'button': [{ 'ref': 'btn', 'disabled': False }] }] })
-                while True:
-                    print(receive())
-                    send({ 'tag': 'Performed', 'contents': { 'foo': [{ 'disabled': False }], 'bar': [{ 'disabled': False }], 'button': [{ 'ref': 'btn', 'disabled': False }] } })
+                def run_sessions():
+                    while True:
+                        msg = receive()
+                        if msg['tag'] == 'Start':
+                            send({ 'tag': 'Event', 'contents': [{ 'tag': 'Loaded' }, { 'foo': [{ 'disabled': False }], 'bar': [{ 'disabled': False }], 'button': [{ 'ref': 'btn', 'disabled': False }] }] })
+                            await_session_commands()
+                        elif msg['tag'] == 'Done':
+                            return msg['results']
+
+                def await_session_commands():
+                    while True:
+                        msg = receive()
+                        if msg['tag'] == 'RequestAction':
+                            send({ 'tag': 'Performed', 'contents': { 'foo': [{ 'disabled': False }], 'bar': [{ 'disabled': False }], 'button': [{ 'ref': 'btn', 'disabled': False }] } })
+                        elif msg['tag'] == 'End':
+                            return
+
+                results = run_sessions()
+                print("Results:")
+                for result in results:
+                    print(f" - {result['valid']['tag']} {result['valid']['contents']}")
