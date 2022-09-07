542b48ebf764e00bfcd865bc03834c49613273cf
Oskar Wickström <oskar@wickstrom.tech>
2022-09-07 21:46:11 +0200

    Remove intentional bug in integration test

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball
-  "https://github.com/NixOS/nixpkgs/archive/4afca382d80b68bff9e154a97210e5a7bf5df8b3.tar.gz") {
+  "https://github.com/NixOS/nixpkgs/archive/67e45078141102f45eff1589a831aeaa3182b41e.tar.gz") {
     config.allowUnfree = true;
   }
