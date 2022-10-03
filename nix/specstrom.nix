8e69ea22f753864043d3abca10b82418767339e2
Oskar Wickström <oskar@wickstrom.tech>
2022-10-03 16:30:15 +0200

    Bump specstrom

--- nix/specstrom.nix
+++ nix/specstrom.nix
@@ -1,3 +1,3 @@
 (import (fetchTarball
-  "https://github.com/quickstrom/specstrom/archive/8ee871ee7c81de6586f191034bb8c233685e9c93.tar.gz") {
+  "https://github.com/quickstrom/specstrom/archive/314790d3d7f6f1ea8abb23caeedbf394c462f219.tar.gz") {
   }).specstrom
